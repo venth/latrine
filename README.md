4f4025c20e4cacf265599adb62bc2c0775b9d88f
Artur Krysiak <artur.krysiak.warszawa@gmail.com>
2017-01-20 19:50:58 +0100

    improved on readme by exaplaining reason and usage; added shitty shit annotation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Latrine
 The purpose is to easier documentation anything you would like flush with water and you're forced to live with it for a while.
+
+## Idea
+The project's idea was born in a head of one of my colleagues during hacking on shitty library
+to remove some of its features that prevented an application to bootstrap properly.
+
+He marked the code with shitty shit comment and explanation. After few more plumbing works team
+decided to improve on marking such code by usage of @ShittyShit annotation.
+
+## Usage
+If you're about to use this annotation, I feel sorry for you. Nevertheless please do not hesitate yourself to apply it to shitty code, which purpose to pull you out of the deep shit you're in.
+
+Just remember to provide explanation, otherwise he might think that you dragged him into a deep shit.
---
+++ src/main/java/org/venth/tools/latrine/ShittyShit.java
@@ -0,0 +1,30 @@
+package org.venth.tools.latrine;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to annotate anything you did to make your application work and it's
+ * shitty.
+ * Created on 20/01/2017.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+        ElementType.ANNOTATION_TYPE,
+        ElementType.CONSTRUCTOR,
+        ElementType.FIELD,
+        ElementType.LOCAL_VARIABLE,
+        ElementType.METHOD,
+        ElementType.PACKAGE,
+        ElementType.PARAMETER,
+        ElementType.TYPE,
+        ElementType.TYPE_PARAMETER,
+        ElementType.TYPE_USE
+})
+public @interface ShittyShit {
+    String value() default "Please provide explanation here";
+}
